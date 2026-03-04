6d595c2957173d3e4b3bb83fb7790455e2fafb7f
Bruane84 <34789033+Bruane84@users.noreply.github.com>
2026-03-04 15:17:54 +0800

    Add files via upload

---
+++ GroupFinderForm.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace TeklaGroupFinder
+{
+    partial class GroupFinderForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.projectNumberTextBox = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.toleranceTextBox = new System.Windows.Forms.TextBox();
+            this.createSignatureButton = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.createSubAssemblyCheckBox = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.udaNameTextBox = new System.Windows.Forms.TextBox();
+            this.findMatchesButton = new System.Windows.Forms.Button();
+            this.openCatalogueButton = new System.Windows.Forms.Button();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.connectionStatusPanel = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Project Number:";
+            // 
+            // projectNumberTextBox
+            // 
+            this.projectNumberTextBox.Location = new System.Drawing.Point(101, 23);
+            this.projectNumberTextBox.Name = "projectNumberTextBox";
+            this.projectNumberTextBox.Size = new System.Drawing.Size(100, 20);
+            this.projectNumberTextBox.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.toleranceTextBox);
+            this.groupBox1.Controls.Add(this.createSignatureButton);
+            this.groupBox1.Location = new System.Drawing.Point(20, 68);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(182, 156);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "1. Create Signature File";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(42, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Tolerance (mm):";
+            // 
+            // toleranceTextBox
+            // 
+            this.toleranceTextBox.Location = new System.Drawing.Point(36, 48);
+            this.toleranceTextBox.Name = "toleranceTextBox";
+            this.toleranceTextBox.Size = new System.Drawing.Size(100, 20);
+            this.toleranceTextBox.TabIndex = 1;
+            this.toleranceTextBox.Text = ".5";
+            // 
+            // createSignatureButton
+            // 
+            this.createSignatureButton.Location = new System.Drawing.Point(15, 91);
+            this.createSignatureButton.Name = "createSignatureButton";
+            this.createSignatureButton.Size = new System.Drawing.Size(150, 46);
+            this.createSignatureButton.TabIndex = 0;
+            this.createSignatureButton.Text = "Create Signature from Selection";
+            this.createSignatureButton.UseVisualStyleBackColor = true;
+            this.createSignatureButton.Click += new System.EventHandler(this.createSignatureButton_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.createSubAssemblyCheckBox);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.udaNameTextBox);
+            this.groupBox2.Controls.Add(this.findMatchesButton);
+            this.groupBox2.Location = new System.Drawing.Point(20, 230);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(217, 191);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "2. Find & Group Matches";
+            // 
+            // createSubAssemblyCheckBox
+            // 
+            this.createSubAssemblyCheckBox.AutoSize = true;
+            this.createSubAssemblyCheckBox.Location = new System.Drawing.Point(15, 163);
+            this.createSubAssemblyCheckBox.Name = "createSubAssemblyCheckBox";
+            this.createSubAssemblyCheckBox.Size = new System.Drawing.Size(199, 17);
+            this.createSubAssemblyCheckBox.TabIndex = 6;
+            this.createSubAssemblyCheckBox.Text = "Create Sub-Assemblies Automatically";
+            this.createSubAssemblyCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(32, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "UDA Name to Write:";
+            // 
+            // udaNameTextBox
+            // 
+            this.udaNameTextBox.Location = new System.Drawing.Point(15, 110);
+            this.udaNameTextBox.Name = "udaNameTextBox";
+            this.udaNameTextBox.Size = new System.Drawing.Size(150, 20);
+            this.udaNameTextBox.TabIndex = 4;
+            this.udaNameTextBox.Text = "SUB_ASSEMBLY_ID";
+            // 
+            // findMatchesButton
+            // 
+            this.findMatchesButton.Location = new System.Drawing.Point(15, 31);
+            this.findMatchesButton.Name = "findMatchesButton";
+            this.findMatchesButton.Size = new System.Drawing.Size(150, 46);
+            this.findMatchesButton.TabIndex = 0;
+            this.findMatchesButton.Text = "Find Matches Using Signature";
+            this.findMatchesButton.UseVisualStyleBackColor = true;
+            this.findMatchesButton.Click += new System.EventHandler(this.findMatchesButton_Click);
+            // 
+            // openCatalogueButton
+            // 
+            this.openCatalogueButton.Location = new System.Drawing.Point(20, 437);
+            this.openCatalogueButton.Name = "openCatalogueButton";
+            this.openCatalogueButton.Size = new System.Drawing.Size(182, 51);
+            this.openCatalogueButton.TabIndex = 4;
+            this.openCatalogueButton.Text = "Open Signature Catalogue";
+            this.openCatalogueButton.UseVisualStyleBackColor = true;
+            this.openCatalogueButton.Click += new System.EventHandler(this.openCatalogueButton_Click);
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Location = new System.Drawing.Point(2, 513);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(41, 13);
+            this.statusLabel.TabIndex = 5;
+            this.statusLabel.Text = "Ready.";
+            // 
+            // connectionStatusPanel
+            // 
+            this.connectionStatusPanel.BackColor = System.Drawing.Color.Red;
+            this.connectionStatusPanel.Location = new System.Drawing.Point(233, 491);
+            this.connectionStatusPanel.Name = "connectionStatusPanel";
+            this.connectionStatusPanel.Size = new System.Drawing.Size(16, 16);
+            this.connectionStatusPanel.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(2, 491);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Connection Status:";
+            // 
+            // GroupFinderForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(261, 532);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.connectionStatusPanel);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.openCatalogueButton);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.projectNumberTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "GroupFinderForm";
+            this.ShowInTaskbar = true;
+            this.Text = "Group Finder";
+            this.TopMost = true;
+            //this.Load += new System.EventHandler(this.GroupFinderForm_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox projectNumberTextBox;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button createSignatureButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox toleranceTextBox;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button findMatchesButton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox udaNameTextBox;
+        private System.Windows.Forms.Button openCatalogueButton;
+        private System.Windows.Forms.Label statusLabel;
+        private System.Windows.Forms.CheckBox createSubAssemblyCheckBox;
+        private System.Windows.Forms.Panel connectionStatusPanel;
+        private System.Windows.Forms.Label label4;
+    }
+}
---
+++ GroupFinderForm.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using Tekla.Structures.Model;
+using Tekla.Structures.Model.UI;
+using Tekla.Structures.Geometry3d;
+
+namespace TeklaGroupFinder
+{
+    public partial class GroupFinderForm : Form
+    {
+        private Model MyModel;
+
+        public GroupFinderForm()
+        {
+            InitializeComponent();
+
+            try
+            {
+                Model MyModel = new Model();
+                if (MyModel.GetConnectionStatus())
+                {
+                    connectionStatusPanel.BackColor = System.Drawing.Color.Green;
+                    statusLabel.Text = "Connected to: " + MyModel.GetInfo().ModelName;
+                }
+                else
+                {
+                    connectionStatusPanel.BackColor = System.Drawing.Color.Red;
+                    statusLabel.Text = "Not connected. Please open a Tekla model.";
+                }
+            }
+            catch (Exception ex)
+            {
+                connectionStatusPanel.BackColor = System.Drawing.Color.Red;
+                statusLabel.Text = "Connection failed.";
+                MessageBox.Show("Error connecting to Tekla: " + ex.Message, "Connection Error");
+            }
+        }
+
+        private void createSignatureButton_Click(object sender, EventArgs e)
+        {
+            if (!MyModel.GetConnectionStatus())
+            {
+                MessageBox.Show("Not connected to a Tekla Structures model.", "Connection Error");
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(projectNumberTextBox.Text))
+            {
+                MessageBox.Show("Please enter a Project Number first.", "Input Required");
+                return;
+            }
+
+            statusLabel.Text = "Select parts in the model...";
+            this.Refresh();
+            var picker = new Picker();
+            ModelObjectEnumerator selectedObjects;
+            try
+            {
+                selectedObjects = picker.PickObjects(Picker.PickObjectsEnum.PICK_N_PARTS,
+                                                     "Select the sample group of parts");
+            }
+            catch (Exception)
+            {
+                statusLabel.Text = "Connected to: " + MyModel.GetInfo().ModelName;
+                return;
+            }
+            if (selectedObjects.GetSize() < 2)
+            {
+                MessageBox.Show("Please select at least 2 parts.", "Selection Error");
+                statusLabel.Text = "Connected to: " + MyModel.GetInfo().ModelName;
+                return;
+            }
+
+            var partsData = new List<Tuple<Part, Tekla.Structures.Geometry3d.Point>>();
+            while (selectedObjects.MoveNext())
+            {
+                if (selectedObjects.Current is Part part)
+                {
+                    partsData.Add(new Tuple<Part, Tekla.Structures.Geometry3d.Point>(part, GetPartCog(part)));
+                }
+            }
+
+            var groupName = Prompt("Enter Signature Name",
+                                   "Please enter a name for this group signature:",
+                                   "Group_Signature");
+            if (string.IsNullOrEmpty(groupName)) return;
+
+            var signature = new GroupSignature
+            {
+                GroupName = groupName,
+                PartCount = partsData.Count,
+                Tolerance = Convert.ToDouble(toleranceTextBox.Text),
+                Parts = partsData.Select(p => GetPartProperties(p.Item1)).ToList(),
+                Distances = CalculateSortedDistances(partsData.Select(p => p.Item2).ToList())
+            };
+
+            var projectNumber = projectNumberTextBox.Text;
+            var pluginDataBasePath = @"C:\TeklaGroupFinderData";
+            var projectFolderPath = Path.Combine(pluginDataBasePath, projectNumber);
+            Directory.CreateDirectory(projectFolderPath);
+            var filePath = Path.Combine(projectFolderPath, $"{groupName}.json");
+            File.WriteAllText(filePath, JsonConvert.SerializeObject(signature, Formatting.Indented));
+
+            statusLabel.Text = "Connected to: " + MyModel.GetInfo().ModelName;
+            MessageBox.Show($"Successfully created signature file:\n{filePath}", "Success");
+        }
+
+        private void findMatchesButton_Click(object sender, EventArgs e)
+        {
+            if (MyModel == null || !MyModel.GetConnectionStatus())
+            {
+                MessageBox.Show("Not connected to a Tekla Structures model.", "Connection Error");
+                return;
+            }
+
+            MessageBox.Show("For this demonstration, please select a group of parts to form a sub-assembly.",
+                            "Demonstration Step");
+
+            var picker = new Picker();
+            ModelObjectEnumerator aGroupToProcess;
+            try
+            {
+                aGroupToProcess = picker.PickObjects(Picker.PickObjectsEnum.PICK_N_PARTS,
+                                                     "Select parts to form a sub-assembly");
+            }
+            catch (Exception)
+            {
+                statusLabel.Text = "Connected to: " + MyModel.GetInfo().ModelName;
+                return;
+            }
+
+            if (aGroupToProcess.GetSize() > 1 && createSubAssemblyCheckBox.Checked)
+            {
+                var partsToGroup = new ArrayList();
+                Part mainPart = null;
+                while (aGroupToProcess.MoveNext())
+                {
+                    if (aGroupToProcess.Current is Part part)
+                    {
+                        partsToGroup.Add(part);
+                        if (mainPart == null) mainPart = part;
+                    }
+                }
+
+                var parentAssembly = mainPart?.GetAssembly();
+                if (parentAssembly == null)
+                {
+                    MessageBox.Show("Could not find the parent assembly.", "Error");
+                    return;
+                }
+
+                var newSubAssembly = new Assembly { Name = "GROUP_ASSEMBLY" };
+                newSubAssembly.SetMainPart(mainPart);
+                for (int i = 1; i < partsToGroup.Count; i++)
+                {
+                    newSubAssembly.Add(partsToGroup[i] as Part);
+                }
+
+                if (!newSubAssembly.Insert())
+                {
+                    MessageBox.Show("Failed to create sub-assembly.", "Error");
+                }
+                else
+                {
+                    parentAssembly.Modify();
+                }
+            }
+
+            MyModel.CommitChanges("Sub-assemblies created by Group Finder application.");
+            statusLabel.Text = "Connected to: " + MyModel.GetInfo().ModelName;
+            MessageBox.Show("Demonstration finished. Parts have been grouped.", "Process Complete");
+        }
+
+        private void openCatalogueButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("The Catalogue will be built in a later step.", "Not Implemented");
+        }
+
+        #region Helper Methods
+        private Tekla.Structures.Geometry3d.Point GetPartCog(Part part)
+        {
+            double cogX = 0, cogY = 0, cogZ = 0;
+            part.GetReportProperty("COG_X", ref cogX);
+            part.GetReportProperty("COG_Y", ref cogY);
+            part.GetReportProperty("COG_Z", ref cogZ);
+            return new Tekla.Structures.Geometry3d.Point(cogX, cogY, cogZ);
+        }
+
+        private Dictionary<string, string> GetPartProperties(Part part)
+        {
+            var props = new Dictionary<string, string>();
+            string profile = "", grade = "";
+            part.GetReportProperty("PROFILE", ref profile);
+            part.GetReportProperty("MATERIAL_GRADE", ref grade);
+            props.Add("Profile", profile);
+            props.Add("Grade", grade);
+            return props;
+        }
+
+        private List<double> CalculateSortedDistances(List<Tekla.Structures.Geometry3d.Point> cogs)
+        {
+            var distances = new List<double>();
+            for (int i = 0; i < cogs.Count; i++)
+            {
+                for (int j = i + 1; j < cogs.Count; j++)
+                {
+                    distances.Add(Math.Round(
+                        Tekla.Structures.Geometry3d.Distance.PointToPoint(cogs[i], cogs[j]), 2));
+                }
+            }
+            distances.Sort();
+            return distances;
+        }
+
+        public static string Prompt(string title, string promptText, string defaultValue)
+        {
+            Form prompt = new Form()
+            {
+                Width = 500,
+                Height = 150,
+                FormBorderStyle = FormBorderStyle.FixedDialog,
+                Text = title,
+                StartPosition = FormStartPosition.CenterScreen
+            };
+            Label textLabel = new Label() { Left = 50, Top = 20, Text = promptText, Width = 400 };
+            TextBox textBox = new TextBox() { Left = 50, Top = 50, Width = 400, Text = defaultValue };
+            Button confirmation = new Button()
+            {
+                Text = "Ok",
+                Left = 350,
+                Width = 100,
+                Top = 80,
+                DialogResult = DialogResult.OK
+            };
+            confirmation.Click += (sender, e) => { prompt.Close(); };
+            prompt.Controls.Add(textBox);
+            prompt.Controls.Add(confirmation);
+            prompt.Controls.Add(textLabel);
+            prompt.AcceptButton = confirmation;
+            return prompt.ShowDialog() == DialogResult.OK ? textBox.Text : "";
+        }
+        #endregion
+    }
+
+    public class GroupSignature
+    {
+        public string GroupName { get; set; }
+        public int PartCount { get; set; }
+        public List<Dictionary<string, string>> Parts { get; set; }
+        public List<double> Distances { get; set; }
+        public double Tolerance { get; set; }
+    }
+}
