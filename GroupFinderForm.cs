5b5033c6fc912ccfddffba30089e28fc9cdfa2b0
Bruane84 <34789033+Bruane84@users.noreply.github.com>
2026-03-04 20:59:28 +0800

    Updated GroupFinderForm.cs with new implementation.

--- GroupFinderForm.cs
+++ GroupFinderForm.cs
@@ -18,13 +18,11 @@ namespace TeklaGroupFinder
         {
             InitializeComponent();
             MyModel = new Model();
-            if (MyModel.GetConnectionStatus())
-            {
+            if (MyModel.GetConnectionStatus()){
                 connectionStatusPanel.BackColor = System.Drawing.Color.Green;
                 statusLabel.Text = "Connected to: " + MyModel.GetInfo().ModelName;
             }
-            else
-            {
+            else{
                 connectionStatusPanel.BackColor = System.Drawing.Color.Red;
                 statusLabel.Text = "Not connected. Please open a Tekla model.";
             }
@@ -81,7 +79,7 @@ namespace TeklaGroupFinder
         }
 
         private const double ProximityLimitMm = 200.0;
-        private const double VolumeToleranceMm3 = 0.5;
+        private const double VolumeToleranceMm3 = 1000.0;
         private const int ProgressUpdateInterval = 25;
 
         private class PlateCache
@@ -244,7 +242,7 @@ namespace TeklaGroupFinder
             return true;
         }
 
-        private string GetDataPath() => Path.Combine("J:\\", projectNumberTextBox.Text, "600 QA and QC", "608 Internal Documents", "MegaPanelPreFabGroup");
+        private string GetDataPath() => Path.Combine("J:\", projectNumberTextBox.Text, "600 QA and QC", "608 Internal Documents", "MegaPanelPreFabGroup");
 
         private string GetProperty(Part p, string n) { string v = ""; p.GetReportProperty(n, ref v); return v; }
 
@@ -326,4 +324,3 @@ namespace TeklaGroupFinder
         public int PartCount { get; set; }
         public double DistanceTolerance { get; set; }
     }
-}
